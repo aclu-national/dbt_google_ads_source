ec907e1c0f35a3847e2925b41fe11decf756d92e
Benjamin Sorensen <bchangsorensen@gmail.com>
2022-09-13 12:23:30 -0400

    build custom model

---
+++ macros/get_campaign_network_state_stats_columns.sql
@@ -0,0 +1,19 @@
+{% macro get_campaign_network_state_stats_columns() %}
+
+{% set columns = [
+    {"name": "customer_id", "datatype": dbt_utils.type_int()},
+    {"name": "date", "datatype": "date"},
+    {"name": "campaign_id", "datatype": dbt_utils.type_int()},
+    {"name": "campaign_name", "datatype": dbt_utils.type_string()},
+    {"name": "clicks", "datatype": dbt_utils.type_int()},
+    {"name": "cost_micros", "datatype": dbt_utils.type_int()},
+    {"name": "impressions", "datatype": dbt_utils.type_int()},
+    {"name": "ad_network_type", "datatype": dbt_utils.type_string()},
+    {"name": "geo_target_state", "datatype": dbt_utils.type_string()}
+] %}
+
+{{ fivetran_utils.add_pass_through_columns(columns, var('google_ads__ad_stats_passthrough_metrics')) }}
+
+{{ return(columns) }}
+
+{% endmacro %}
---
+++ models/stg_google_ads__campaign_network_state_stats.sql
@@ -0,0 +1,51 @@
+{{ config(enabled=var('ad_reporting__google_ads_enabled', True)) }}
+
+with base as (
+
+    select *
+    from {{ ref('stg_google_ads__campaign_network_state_stats_tmp') }}
+
+),
+
+fields as (
+
+    select
+        {{
+            fivetran_utils.fill_staging_columns(
+                source_columns=adapter.get_columns_in_relation(ref('stg_google_ads__campaign_network_state_stats_tmp')),
+                staging_columns=get_ad_stats_columns()
+            )
+        }}
+
+    from base
+),
+
+states as (
+
+    select
+      *
+    from {{ ref('google_ads_state_mapping') }}
+
+)
+
+final as (
+
+    select
+        fields.customer_id as account_id,
+        fields.campaign_id,
+        fields.campaign_name,
+        fields.date as date_day,
+        fields.ad_network_type,
+        states.state_name,
+        fields.customer_currency_code,
+        fields.clicks,
+        fields.cost_micros / 1000000.0 as spend,
+        fields.impressions
+        {{ fivetran_utils.fill_pass_through_columns('google_ads__ad_stats_passthrough_metrics') }}
+
+    from fields
+    left join states on
+      regexp_substr(fields.geo_target_state, '\\d+') = states.geo_target_state_id
+)
+
+select * from final
